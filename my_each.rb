3b8774a910364d3b9aa44a09c5039ae991fde175
github username <github email address>
2017-05-22 13:40:14 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array1) # put argument(s) here
+# code here
+  i = 0
+  while i <= (array1.count - 1)
+    yield array1[i]
+    i+=1
+  end
+  array1
 end
+
+array1 = [1,8,7,4,77,45,23]
+my_each(array1) {|a| print a.to_s + "\n"}
